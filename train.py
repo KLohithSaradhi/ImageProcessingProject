052b94168466624695ade17e78a5e2798204c2bf
Lohith <lohith21110257@snuchennai.edu.in>
2024-03-04 22:34:49 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,33 +1,50 @@
-import utils
 import model
+import utils
+import argparse
+
+
 import torch
 from torch.utils.data import DataLoader
 from torch.optim import Adam
 
 from sklearn.model_selection import train_test_split
 
-PATH_PAIRS = utils.get_file_paths("./AugmentedData/hazy/", "./AugmentedData/clear/")
 
-train_paths, test_paths = train_test_split(PATH_PAIRS, test_size=0.2)
 
-train_dataset = utils.HeyZee(train_paths)
-test_dataset = utils.HeyZee(test_paths)
+parser = argparse.ArgumentParser(prog="train.py", description="Train a model")
+parser.add_argument("--data", type=str, help="Path to the data")
+parser.add_argument("--model", type=str, default=None, help="Path to the model")
+parser.add_argument("--epochs", type=int, default=1, help="Number of epochs")
+parser.add_argument("--batch_size", type=int, default=4, help="Batch size")
+parser.add_argument("--lr", type=float, default=0.0001, help="Learning rate")
+parser.add_argument("--b1", type=float, default=0.5, help="beta 1")
+parser.add_argument("--b2", type=float, default=0.999, help="beta 2")
+parser.add_argument("--weight_decay", type=float, default=1e-4, help="weight decay")
 
-BATCH_SIZE = 4
+args = parser.parse_args()
 
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+path_pairs = utils.get_file_paths(args.data + "hazy", args.data + "clear")
+
+
+train_pairs, test_pairs = train_test_split(path_pairs, test_size=0.2, random_state=42)
+
+train_dataset = utils.HeyZee(train_pairs)
+test_dataset = utils.HeyZee(test_pairs)
+
+
+train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True)
 
 TheHayZee = model.MASK()
+
 TheHayZee = TheHayZee.cuda()
 
 loss = utils.TotalLoss()
-optim = Adam(TheHayZee.parameters(), lr=5e-4, betas = (0.5, 0.999), weight_decay=0.0001)
-
-EPOCHS = 10
+optim = Adam(TheHayZee.parameters(), lr=args.lr, betas = (args.b1, args.b2), weight_decay=args.weight_decay)
 
 
-for e in range(EPOCHS):
+for e in range(args.epochs):
 
     for i, (haze, clear) in enumerate(train_loader):
 
